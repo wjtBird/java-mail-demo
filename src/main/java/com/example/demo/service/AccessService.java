04b047be09168b64c476a6aa8c9650a39e370001
jintao.wang001 <jintao.wang001@sino-life.com>
2018-03-12 12:47:38 +0800

    init commit

---
+++ src/main/java/com/example/demo/service/AccessService.java
@@ -0,0 +1,93 @@
+package com.example.demo.service;
+
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.PropertySet;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.enumeration.property.WellKnownFolderName;
+import microsoft.exchange.webservices.data.core.service.folder.Folder;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+import microsoft.exchange.webservices.data.core.service.item.Item;
+import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
+import microsoft.exchange.webservices.data.credential.WebCredentials;
+import microsoft.exchange.webservices.data.property.complex.EmailAddress;
+import microsoft.exchange.webservices.data.property.complex.MessageBody;
+import microsoft.exchange.webservices.data.search.FindItemsResults;
+import microsoft.exchange.webservices.data.search.ItemView;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class AccessService {
+
+
+
+    public ExchangeService getService(String account,String password) throws Exception {
+        ExchangeService exchangeService = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+        ExchangeCredentials credentials = new WebCredentials(account, password);
+        exchangeService.setCredentials(credentials);
+        exchangeService.autodiscoverUrl(account,(a)->(a.toLowerCase().startsWith("https://")));
+        return exchangeService;
+    }
+
+
+    public void listFirstTenItems(String account,String password) throws Exception {
+        ExchangeService service = this.getService(account, password);
+        Folder inbox = Folder.bind(service, WellKnownFolderName.Inbox);
+        ItemView view = new ItemView(10);
+        FindItemsResults<Item> findResults = service.findItems(inbox.getId(), view);
+
+        //MOOOOOOST IMPORTANT: load messages' properties before
+        service.loadPropertiesForItems(findResults, PropertySet.FirstClassProperties);
+
+        for (Item item : findResults.getItems()) {
+
+
+            // Do something with the item as shown
+            System.out.println("id==========" + item.getId());
+            System.out.println("sub==========" + item.getSubject());
+        }
+    }
+
+    public void readMeg(String account,String password) throws Exception {
+        ExchangeService service = this.getService(account, password);
+        Folder inbox = Folder.bind(service, WellKnownFolderName.Inbox);
+        ItemView view = new ItemView(10);
+        FindItemsResults<Item> findResults = service.findItems(inbox.getId(), view);
+
+        for (Item item : findResults) {
+            EmailMessage message = (EmailMessage) item;
+            String subject = message.getSubject();
+            System.out.println("isread:"+message.getIsRead());
+            if ("send user by ews api".equals(subject)) {
+                System.out.println("send user by ews api is read");
+                message.setIsRead(true);
+                message.send();
+            }
+            System.out.println("subject："+message.getSubject());
+            System.out.println("from:"+message.getFrom());
+//            System.out.println("msgBody:"+message.getBody().getBodyType());
+//            System.out.println(item.getBody());
+        }
+
+
+    }
+
+
+    public void sendMsg(String account,String password) throws Exception {
+        ExchangeService service = this.getService(account, password);
+
+        EmailMessage message = new EmailMessage(service);
+
+        message.setFrom(EmailAddress.getEmailAddressFromString(account));
+        message.getToRecipients().add(EmailAddress.getEmailAddressFromString(account));
+        message.setSubject("send user by ews api");
+        message.setBody(MessageBody.getMessageBodyFromText("hi,this is a bug"));
+
+        message.send();
+
+    }
+
+
+
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import com.example.demo.service.AccessService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Autowired
+	private AccessService accessService;
+
+	private String account = "jintao.wang001@sino-life.com";
+
+	private String password = "Sinolife1";
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void listFirstTenItemsTest() throws Exception {
+		this.accessService.listFirstTenItems("jintao.wang001@sino-life.com","Sinolife1");
+	}
+
+	@Test
+	public void sendMsgTest() throws Exception {
+		this.accessService.sendMsg(this.account,this.password);
+	}
+
+	@Test
+	public void readMsgTest() throws Exception {
+		this.accessService.readMeg(this.account,this.password);
+	}
+
+}
